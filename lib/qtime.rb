114d99b89c3472e1ff4d2293fd9e67bd238bb5e3
mike.gao <mike.gao0611@gmail.com>
2014-09-15 16:22:30 +0800

    init

---
+++ lib/qtime.rb
@@ -0,0 +1,49 @@
+require "time"
+require "date"
+
+class Numeric
+  def days
+    (self.to_f * 86400).to_i
+  end
+end
+
+class Qtime
+  class << self
+    def sub(time1, time2)
+      if time1.is_a?(Time) and time2.is_a?(Time)
+        sub = time1.to_i - time2.to_i
+        if sub >0
+          return time1.to_i - time2.to_i
+        else
+          raise "parameter 1 is bigger than parameter 2, the sub is #{sub}"
+        end
+      else
+        raise "wrong arguments type, it is need Time"
+      end
+    end
+  end
+end
+
+class Time
+  def -(days)
+    Time.at(self.to_i - days)
+  end
+
+  def normal_parse
+    self.strftime("%Y-%m-%d %H:%S:%M")
+  end
+
+  def to_day(separator="")
+    case separator
+    when "line"
+      self.strftime("%Y-%m-%d")
+    when "solidus"
+      self.strftime("%Y/%m/%d")
+    else
+      self.strftime("%Y%m%d")
+    end
+  end
+
+end
+
+p Time.new.to_day
---
+++ qtime.gemspec
@@ -0,0 +1,12 @@
+Gem::Specification.new do |s|
+  s.name          = 'qtime'
+  s.version       = '0.0.1'
+  s.date          = '2014-06-10'
+  s.summary       = 'extesion time and date classs'
+  s.description   = 'A gem for extesion time and date class'
+  s.authors       = ['Mike.Gao']
+  s.email         = 'mike.gao0611@gmail.com'
+  s.files         = ['lib/qtime.rb']
+  s.homepage      = 'https://github.com/mikegao/qtime'
+  s.license       = 'MIT'
+end
